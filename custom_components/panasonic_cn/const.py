2569cc9b4d26fad249f175e16f1c17ed70e8d6f8
delichik <delichik@imiku.moe>
2025-06-28 11:51:25 +0800

    doc: 更新自动化发布

--- custom_components/panasonic_cn/const.py
+++ custom_components/panasonic_cn/const.py
@@ -1,7 +1,7 @@
 """Constants for the Panasonic CN integration."""
 
 # Integration domain name
-DOMAIN = "panasonic"
+DOMAIN = "panasonic_cn"
  
 # Default scan interval in seconds for updating device states
-DEFAULT_SCAN_INTERVAL = 30 
+DEFAULT_SCAN_INTERVAL = 30
